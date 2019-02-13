4da95327d5c6468aad46bc77e1e3de0b8b358a87
Pedro Henrique Rech Sarmento <36777671+kxpedro@users.noreply.github.com>
2019-02-13 05:10:38 -0200

    Update password autentification
    Instalation of crypto, using md5 to secure password

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -1,19 +1,28 @@
+var crypto = require('crypto');
+
 function UsuariosDAO(connection){
     this._connection = connection();    
 };
 
 UsuariosDAO.prototype.inserirUsuario = async function(usuario){
     try {
-        const db = await this._connection;
+        const db = await this._connection;        
+        var senha_crypto = crypto.createHash("md5").update(usuario.senha).digest("hex");
+
+        usuario.senha = senha_crypto;
         await db.collection('usuario').insertOne(usuario);
+
     } catch (error) {
-        console.error('Tivemos um problema no model inserirUsuario');
+        console.error('Tivemos um problema no model inserirUsuario: ', error);
     }
 };
 
 UsuariosDAO.prototype.autenticar = async function(usuario, req, res){
     try {
         const db = await this._connection;
+        var senha_crypto = crypto.createHash("md5").update(usuario.senha).digest("hex");
+        
+        usuario.senha = senha_crypto;
         await db.collection('usuario').find(usuario).toArray(function(err, result){            
             if(result[0] != undefined){
                 req.session.autorizado = true;
